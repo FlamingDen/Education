b47b4dc1d7bca21640b03b3f8331f70e579b1aef
Daniil Basak <basak-da@agat.by>
2024-10-24 11:47:30 +0300

    Объекты в качестве возвщаемого значения

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <compare>
 #include <iostream>
 #include <set>
+#include <string>
  
 struct Point
 {
@@ -9,41 +10,90 @@ struct Point
     auto operator<=>(const Point&) const = default;
     /* non-comparison functions */
 };
+
+class MyClass{
+public:
+    MyClass(): a(0), b(0){};
+    MyClass(int a, int b) : a(a), b(b){};
+    ~MyClass(){}
+    
+    int a;
+    int b;
+};
  
-int main()
-{
-    Point pt1{1, 1}, pt2{1, 2};
-    std::set<Point> s; // OK
-    s.insert(pt1);     // OK
- 
-    // two-way comparison operator functions are not required to be explicitly defined:
-    // operator== is implicitly declared (see below)
-    // the overload resolutions of other candidates will select rewritten candidates 
-    std::cout << std::boolalpha
-        << (pt1 == pt2) << ' ' << std::endl  // false
-        << (pt1 != pt2) << ' ' << std::endl  // true
-        << (pt1 <  pt2) << ' ' << std::endl  // true
-        << (pt1 <= pt2) << ' ' << std::endl  // true
-        << (pt1 >  pt2) << ' ' << std::endl  // false
-        << (pt1 >= pt2) << ' ' << std::endl; // false
+
+
+class B{
+    int b;
+public:
+    B(): b(0){
+        std::cout << "Конструктор B()" << std::endl;
+    }
+    B(int b): b(b){
+        std::cout << "Конструктор B()" << std::endl;
+    }
+    ~B(){
+        std::cout << "Деструктор ~B()" << std::endl;
+    }
+
+    int get_b() const{
+        return b;
+    };
+
+    virtual void show(){
+        std::cout << "B : " << get_b() << std::endl;
+    }
+};
+
+class D: public B{
+    int d;
+public:
+    D(): d(0) {
+        std::cout << "Конструктор D()" << std::endl;
+    }
+    D(int b, int d): B(b), d(d){
+        std::cout << "Конструктор D()" << std::endl;
+    }
+    ~D(){
+        std::cout << "Декструткор ~D()" << std::endl;
+    }
+    int get_d() const{
+        return this->d;
+    };
+    void show() override {
+        std:: cout << "D : " << this->get_b() << "\t" << d << std::endl;
+    }
+
+    int mul() const {
+        return d * get_b();
+    };
+};
+
+template<typename T>
+void print(T curr){
+    std::cout << "print()" << std::endl;
 }
 
+D input(){
+    D temp(4,4);
+    return D(6,6);
+}
 
-// struct Point
-// {
-//     Point(int x, int y){
-//         this->x =x;
-//         this->y = y;
-//     }
+B inputB(){
+    B temp(4);
+    return B(1);
+}
 
-//     int x;
-//     int y;
-// };
- 
-// int main(){
-//     std::cout<< "hello" << std::endl;
-//     Point pt1{1, 1}, pt2{1, 2};
-//     std::set<Point> s; // OK
-//     std::cout << __cplusplus << std::endl;
-//     return 0;
-// }
+int main(int argc, char* argv[])
+{
+    std::cout << argv[0] << std::endl;
+
+    // D x = input();
+    // x.show();
+
+    B y;
+    y = inputB();
+    y.show();
+
+    return 0;
+}
