48c81cd2f57527f0ab1618122e0063ea91aa358e
Daniil Basak <basak-da@agat.by>
2024-12-13 13:46:35 +0300

    Add: timer && show
    - edit CMakeLists.txt
    - add show.h
    - add timer.cpp/.h

---
+++ show.h
@@ -0,0 +1,80 @@
+﻿#pragma once
+
+#include <iostream>
+#include <vector>
+
+namespace sh {
+    template<typename T>
+    void showContainer(const T& c);
+
+    template<typename K, typename V>
+    void showMap(std::map<K, V>& c);
+
+    template<typename T>
+    void showVecVec(const std::vector<std::vector<T>>& v);
+
+    template<typename T>
+    void print(T smt);
+}
+
+
+template<typename T>
+void sh::showContainer(const T& c){
+    if(c.empty()){
+        std::cout << "[ Empty ] \n";
+        return; 
+    }
+    std::cout << "[";
+    for(auto it = std::begin(c); it != std::end(c); ++it){
+        if(std::next(it) == std::end(c)){
+            std::cout << *it <<']'<<  std::endl;
+            break;
+        }
+        std::cout << *it << ", ";
+    }
+}
+
+template<typename K, typename V>
+void sh::showMap(std::map<K, V>& c){
+    if(c.empty()){
+        std::cout << "[ Empty ] \n";
+        return; 
+    }
+    std::cout << "Map = [";
+    for(auto it = std::begin(c); it != std::end(c); ++it){
+        if(std::next(it) == std::end(c)){
+            std::cout << it->first << " - " << it->second << ']'<<  std::endl;
+            break;
+        }
+        std::cout << it->first << " - " << it->second << ", \n";
+        std::cout.width(7);
+        std::cout << "";
+    }
+}
+
+template <typename T>
+void sh::showVecVec(const std::vector<std::vector<T>> &v)
+{
+    if(v.empty()){
+        std::cout << "[[ Empty ]] \n";
+        return; 
+    }
+    std::cout <<'[';
+    for (int i = 0; i < std::ssize(v); i++)
+    {
+        if(i ==  std::ssize(v) - 1){
+            showContainer(v[i]);
+            std::cout << ']' << std::endl;
+            break;
+        }
+        showContainer(v[i]);
+        std::cout << ",";
+    }
+}
+
+template <typename T>
+void sh::print(T smt)
+{
+    std::cout << smt << std::endl;
+}
+
---
+++ timer.cpp
@@ -0,0 +1,33 @@
+﻿#include "timer.h"
+
+Time::Time(){
+    st = std::chrono::high_resolution_clock::now();
+    end = st;
+}
+
+Time::Time(std::string m): message(m) {
+    st = std::chrono::high_resolution_clock::now();
+}
+
+Time::~Time(){
+    end = std::chrono::high_resolution_clock::now();
+    std::cout << std::endl;
+    std::cout << message << "(all) = { "  << std::chrono::duration<double>(end -st) << " }\n";
+}
+
+void Time::End(){
+    end = std::chrono::high_resolution_clock::now(); 
+}
+
+std::chrono::duration<double> Time::GetExcTime()
+{
+    return std::chrono::duration<double>(end-st);
+}
+
+void Time::PrintExcTime(std::string m)
+{
+    if(st  != end){
+        std::cout << std::endl;
+        std::cout << m << " = { "  << std::chrono::duration<double>(end -st) << " }\n";
+    }
+}
